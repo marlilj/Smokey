c706c0a7685def381b94d658eb266c9631374beb
David Andersson <david.m.andersson@gmail.com>
2021-11-29 15:31:20 +0100

    add implementation for getters

--- app/emulator/src/emulator.cpp
+++ app/emulator/src/emulator.cpp
@@ -341,3 +341,56 @@ bool Emulator::GracefulShutdown() {
   error_code = kSuccess;
   return error_code;
 }
+
+size_t EmulatorData::GetThrottleSetValue() {
+  std::lock_guard<std::recursive_mutex> guard(emulator_data_mutex_);
+  return throttle_set_value;
+}
+size_t EmulatorData::GetThrottle() {
+  std::lock_guard<std::recursive_mutex> guard(emulator_data_mutex_);
+  return throttle;
+}
+size_t EmulatorData::GetGearSetValue() {
+  std::lock_guard<std::recursive_mutex> guard(emulator_data_mutex_);
+  return gear_set_value;
+}
+size_t EmulatorData::GetStartSetValue() {
+  std::lock_guard<std::recursive_mutex> guard(emulator_data_mutex_);
+  return start_set_value;
+}
+size_t EmulatorData::GetGear() {
+  std::lock_guard<std::recursive_mutex> guard(emulator_data_mutex_);
+  return gear;
+}
+size_t EmulatorData::GetRpm() {
+  std::lock_guard<std::recursive_mutex> guard(emulator_data_mutex_);
+  return rpm;
+}
+size_t EmulatorData::GetSpeed() {
+  std::lock_guard<std::recursive_mutex> guard(emulator_data_mutex_);
+  return speed;
+}
+size_t EmulatorData::GetForwardForce() {
+  std::lock_guard<std::recursive_mutex> guard(emulator_data_mutex_);
+  return forward_force;
+}
+bool EmulatorData::GetActivateEngine() {
+  std::lock_guard<std::recursive_mutex> guard(emulator_data_mutex_);
+  return activate_engine;
+}
+bool EmulatorData::GetPindleNeutral() {
+  std::lock_guard<std::recursive_mutex> guard(emulator_data_mutex_);
+  return pindle_neutral;
+}
+bool EmulatorData::GetPindleDrive() {
+  std::lock_guard<std::recursive_mutex> guard(emulator_data_mutex_);
+  return pindle_drive;
+}
+bool EmulatorData::GetPindleReverse() {
+  std::lock_guard<std::recursive_mutex> guard(emulator_data_mutex_);
+  return pindle_reverse;
+}
+bool EmulatorData::GetParkingFlag() {
+  std::lock_guard<std::recursive_mutex> guard(emulator_data_mutex_);
+  return parking_flag;
+}
