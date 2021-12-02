2050a6ebbdd95eaaa2d1bd88f54029fd6e373e5d
Daniel Muhr <dmuhr@volvocars.com>
2021-12-02 11:43:10 +0100

    Enabled graceful shutdown in avic as requested from Rashid.

--- app/avic/src/your_stuff.cpp
+++ app/avic/src/your_stuff.cpp
@@ -1,9 +1,7 @@
-//#include <chrono>
-#include <thread>
-#include <iostream>
+#include <thread>  // NOLINT Due to we don't care!
+#include <iostream>  // NOLINT Due to we don't care!
 #include "your_stuff.h"
 #include "smokey_data.hpp"
-//#include "canio/can_common.h"
 uint8_t fuel = 0;
 uint8_t temp = 0;
 uint8_t oil = 0;
@@ -11,27 +9,27 @@ void yourStuff::YouHaveJustRecievedACANFrame(const canfd_frame * const _frame) {
     /* const unsigned short *rpm = reinterpret_cast<const unsigned short*>(_frame->data);
     this->InstrumentCluster.setRPM(*rpm);
     std::cout << "Recieved frame with " << *rpm << std::endl; */
-    
+
     if (_frame->can_id == k_FrameIdUserInput) {
        const Payload_t *ui = reinterpret_cast<const Payload_t*>(_frame->data);
-           
-       
-        
-    } else if (_frame->can_id == k_FrameIdEmulator) { 
-        const EmulatorOutput_t *em = reinterpret_cast<const EmulatorOutput_t*>(_frame->data);
+
+
+
+    } else if (_frame->can_id == k_FrameIdEmulator) {
+        const EmulatorOutput_t *em = reinterpret_cast<const EmulatorOutput_t*>(_frame->data);  // NOLINT Due to line break decreasing readability.
         this->InstrumentCluster.setRPM(em->rpm);
         this->InstrumentCluster.setSpeed(em->speed);
         this->InstrumentCluster.setGear(em->gear);
         this->InstrumentCluster.setGearPindle_int(em->pindle);
         this->InstrumentCluster.ignite(em->start);
-        
+
 
         if (em->start) {
             fuel = (fuel >= 255 - 10) ? 255 : fuel += 10;
              temp = (temp >= 128 - 5) ? 128 : temp += 5;
             oil = (oil >= 128 - 5) ? 128 : oil += 5;
 
-            
+
             this->InstrumentCluster.setTXT("SMOKEY");
             this->InstrumentCluster.setFuelGauges(fuel);
             this->InstrumentCluster.setTemperatureGauges(temp);
@@ -46,79 +44,10 @@ void yourStuff::YouHaveJustRecievedACANFrame(const canfd_frame * const _frame) {
             this->InstrumentCluster.setOilTemperatureGauges(oil);
         }
     }
-
-    /* const unsigned short rpm = _frame->data[0] * 100;
-    const unsigned short spd = _frame->data[0];
-    this->InstrumentCluster.setRPM(rpm);
-    this->InstrumentCluster.setSpeed(spd);
-    if (_frame->data[0] != 0) {
-        this->InstrumentCluster.ignite(true);
-    } else {
-        this->InstrumentCluster.ignite(false);
-    }
-    const unsigned char gear = _frame->data[0]/10;
-    this->InstrumentCluster.setGear(gear);
-    this->InstrumentCluster.setOilTemperatureGauges(128);
-    this->InstrumentCluster.setTemperatureGauges(128);
-    this->InstrumentCluster.setFuelGauges(200); */
-
-
-
-    /* const struct EngineData *eng = reinterpret_cast<const struct EngineData*>(_frame->data);
-    this->InstrumentCluster.setRPM(eng->rpm);
-    this->InstrumentCluster.setSpeed(eng->speed); */
-
-
-   /*  switch (_frame->can_id) {
-    case CAN::MSG::GAUGES_ID: {
-        const struct CAN::MSG::Gauges_t::_inner* s =
-                reinterpret_cast<const struct CAN::MSG::Gauges_t::_inner* >((_frame->data));
-        this->InstrumentCluster.setFuelGauges(s->G_FUEL);
-        this->InstrumentCluster.setTemperatureGauges(s->G_TEMP);
-        this->InstrumentCluster.setOilTemperatureGauges(s->G_OILT);
-        break;
-    }
-    case CAN::MSG::ICONSS_ID:
-        struct _icons  p;// = reinterpret_cast<struct _icons * >((_frame->data));
-        static bool once = false;
-
-        if (!once) {
-            p.hazard = 1;
-            once = true;
-        } else
-            p.hazard = 0;
-        this->InstrumentCluster.setIcon(&p);
-        break;
-//    case CAN::MSG::USERIN_ID: {
-//       // const struct CAN::MSG::_userin *d = reinterpret_cast<const struct CAN::MSG::_userin * >((_frame->data));
-//       // this->InstrumentCluster.ignite(d->IGNT);
-//    }
-//        break;
-
-    case CAN::MSG::GEARBX_ID: {
-        const struct CAN::MSG::Gearbx_t::_bits *d = reinterpret_cast<const struct CAN::MSG::Gearbx_t::_bits * >((_frame->data));
-        this->InstrumentCluster.setGearPindle_int(d->GEAR_P);
-        this->InstrumentCluster.setGear(d->GEAR_N);
-    }
-        break;
-    case CAN::MSG::ENGINE_ID: {
-        const struct CAN::MSG::_engine *d = reinterpret_cast<const struct CAN::MSG::_engine * >((_frame->data));
-        this->InstrumentCluster.ignite(d->RUN);
-        this->InstrumentCluster.setRPM(d->RPM);
-        this->InstrumentCluster.setSpeed(d->SPD);
-
-    }
-        break;
-    default:
-        break;
-    }
- */
 }
 
-/******************************* ANYTHING BELOW THIS LINE IS JUST BORING STUFF *******************************/
-
 yourStuff::yourStuff(const std::string &_ifName, QObject *_vs) {
-    if(!(this->CANReader.open(_ifName))) exit(-1);//emit die();
+    if(!(this->CANReader.open(_ifName))) exit(-1);//emit die();  // NOLINT Due to we don't care!
 
     this->InstrumentCluster.init(_vs);
 
@@ -133,20 +62,19 @@ bool yourStuff::run() {
     /*while*/if (status == CANOpener::ReadStatus::OK) {
         this->YouHaveJustRecievedACANFrame(&frame);
     }
-    if (status == CANOpener::ReadStatus::ERROR) ret = false;
-    else if (status == CANOpener::ReadStatus::NAVAL ||
-             status == CANOpener::ReadStatus::ENDOF) this->Counter++;
-    else   this->Counter = 0;
-    //if (this->Counter > 200) ret = false;
+    if (status == CANOpener::ReadStatus::ERROR) { ret = false;
+  } else if (status == CANOpener::ReadStatus::NAVAL ||
+             status == CANOpener::ReadStatus::ENDOF) {
+               this->Counter++;
+  }
+    // else   this->Counter = 0;
+    if (this->Counter > 200) ret = false;
     return ret;
 }
 
 void yourStuff::timerEvent(QTimerEvent*) {
-    if(this->run());
-    else {
-//    emit this->die();
+    if(this->run()); { // NOLINT Due to we don't care!
+    } else {
         exit(-1);
     }
-
 }
-
